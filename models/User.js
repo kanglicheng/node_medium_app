9a3f976ecab7cde69814fe66341129fdba414df2
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-04-22 22:25:26 -0500

    add set password and password validation methods

--- models/User.js
+++ models/User.js
@@ -11,6 +11,14 @@ var UserSchema = new mongoose.Schema({
   salt: String
   }, {timestampes:true});
 
-  UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
-  mongoose.model('User', UserSchema);
+UserSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+mongoose.model('User', UserSchema);
