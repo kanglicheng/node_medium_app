9fc35b16db27b6950200268af7f557b1085f2c8a
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-04-23 02:39:27 -0500

    create registration route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,15 @@
 var router = require('express').Router();
+router.use('/', require('./users'));
+
+router.post('/users', function(req, res, next)){
+  var user= new User();
+user.username = req.body.user.username;
+user.email = req.body.user.email;
+user.setPassword(req.body.user.password);
+
+user.save().then(function(){
+  return res.json({user: user.toAuthJSON()});
+}).catch(next);
+});
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var router = require('express').Router();
+var passport = require('passport');
+var User = mongoose.model('User');
+var auth = require('../auth');
+
+module.exports = router;
